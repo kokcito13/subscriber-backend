974822f8ebd0d58b7a7d8ce8a73243bf7e0a8baa
Oleksandr Klosovych <oklosovych@cbhbank.com>
2026-02-16 16:13:25 +0100

    chore(logging): route prod logs to var/log, set action_level to INFO, add test endpoint logging; install monolog

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class TestController extends AbstractController
 {
     #[Route('/test', name: 'app_test')]
-    public function test(): JsonResponse
+    public function test(LoggerInterface $logger): JsonResponse
     {
+        $logger->info('Test endpoint called', ['path' => '/test']);
+
         return new JsonResponse([
             'status' => 'success',
             'message' => 'Symfony application is working!',
